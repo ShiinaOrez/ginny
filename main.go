3ab803be0feafd27c93a0842c3722a84c0aca64d
ShiinaOrez <shiina_orez@qq.com>
2019-09-11 23:42:47 +0800

    [update 0.1.2]

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type RegisterPayload struct {
+	Username  string  `json:"username"`
+	Password  string  `json:"password"`
+}
+
 func main() {
-    router := gin.Default()
-    router.GET("/hello", func(c *gin.Context) {
-        name, ok := c.GetQuery("name")
-        if !ok {
+	router := gin.Default()
+	router.POST("/register", func(c *gin.Context) {
+        var data RegisterPayload
+        if err := c.BindJSON(&data); err != nil {
         	c.JSON(400, gin.H{
-                "message": "Bad Request!",
+                "message": "Bad Request", // 如果绑定失败，那么我们认定它为一个坏请求，按照规范，状态码应该为400
         	})
-                return
+        	return
         }
         c.JSON(200, gin.H{
-                "message": "Hello, " + name + "!",
+            "result": data.Username + data.Password,
         })
-    })
+        return
+	})
 
-    router.Run()
+	router.Run()
 }
